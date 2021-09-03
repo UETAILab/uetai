6198103bcb57585ff0292a993d8dc09b280326b3
manhdung20112000 <manhdung20112000@gmail.com>
2021-09-03 21:02:45 +0700

    fix pep8

--- src/uetai/logger/__init__.py
+++ src/uetai/logger/__init__.py
@@ -1,5 +1,4 @@
 """init public class"""
 from .summary_writer import SummaryWriter
-from .callbacks.image_classifier import ImageMonitorBase
 
-__all__ = ["SummaryWriter", "ImageMonitorBase"]
+__all__ = ["SummaryWriter"]

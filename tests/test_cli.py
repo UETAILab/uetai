3a02d4e14414086448ce9f25b48730060e64c885
Nguyen Văn Phi <5081237-gungui@users.noreply.gitlab.com>
2021-09-05 22:14:37 +0700

    fix test fail

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,38 +2,34 @@ __author__ = "Nguyen Van Phi"
 __copyright__ = "Nguyen Van Phi"
 __license__ = "MIT"
 
+from unittest import TestCase
+
 import torch
-from uetai.logger.wandb.wandb_logger import WandbLogger, create_dataset_artifact, download_model_artifact
+from uetai.logger.summary_writer import SummaryWriter
 
 
 # from uetai.logger.summary_writer import SummaryWriter
 
+class TestSummaryWriterWandb(TestCase):
+    def test_artifact_control_function(self):
+        logger = SummaryWriter('experiment')
+        # upload (random) dataset as artifact
+        path = 'path/to/dataset'
+        logger.log_dataset_artifact(path, dataset_name='demo-dataset')
 
-def test_artifact_control_function():
-    wb_logger = WandbLogger('experiment')
-    # upload (random) dataset as artifact
-    path = 'path/to/dataset'
-    create_dataset_artifact(path, dataset_name='demo-dataset')
+        # delete local dataset
+        down_path = logger.download_dataset_artifact(dataset_name='demo-dataset')
 
-    # delete local dataset
-    down_path = wb_logger.download_dataset_artifact(dataset_name='demo-dataset')
+        # load dataset
 
-    # load dataset
+        # setup a simple training run
+        model = torch.nn.Linear(10, 10)
+        for epoch in range(100):
+            logger.save_model(model.state_dict(), 'model.pth')
 
-    # setup a simple training run
-    model = torch.nn.Linear(10, 10)
-    for epoch in range(100):
-        torch.save(model.state_dict(), 'model.pth')
-        wb_logger.log_model('model.pth')
+        # delete current model weight
 
-    # delete current model weight
-
-    # attempt to download latest weight and continues train
-    weight_path = download_model_artifact('experiment/model.pth')
-    assert down_path is not None
-    assert weight_path is not None
-    pass
-
-
-def test_main():
-    assert 1 == 1
+        # attempt to download latest weight and continues train
+        weight_path = logger.log_model_artifact('experiment/model.pth')
+        assert down_path is not None
+        assert weight_path is not None

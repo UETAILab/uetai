2d770e8e375e18fc3e9e236cc9afef4813e8b794
Nguyen Văn Phi <5081237-gungui@users.noreply.gitlab.com>
2021-09-09 22:51:31 +0700

    trigger test

--- tests/test_upload_download_artifact.py
+++ tests/test_upload_download_artifact.py
@@ -5,19 +5,21 @@ __license__ = "MIT"
 
 import os
 import shutil
+import time
 from unittest import TestCase
 
 import torch
-from wandb.sdk.wandb_artifacts import Artifact
+from wandb import Artifact
+
 from uetai.logger.summary_writer import SummaryWriter
 
 
 class TestSummaryWriterWandb(TestCase):
     def __init__(self, *args, **kwargs):
         super(TestSummaryWriterWandb, self).__init__(*args, **kwargs)
-        self.logger = SummaryWriter('uetai', log_tool='wandb')
 
     def test_dataset_artifact_upload_download(self):
+        self.logger = SummaryWriter('uetai', log_tool='wandb')
         # create a (random) dataset
         path = './tmp/dataset'
         os.makedirs(path, exist_ok=True)
@@ -26,10 +28,8 @@ class TestSummaryWriterWandb(TestCase):
         # upload dataset as artifact
         artifact = self.logger.log_dataset_artifact(path, dataset_name="dummy-set")
         shutil.rmtree('./tmp')  # del tmpdir
-        assert type(artifact) == Artifact
-
-        down_path, dataset_artifact = self.logger.download_dataset_artifact(
-                                                dataset_name='dummy-set')
-        assert type(dataset_artifact) == Artifact
+        assert isinstance(artifact, Artifact)
+        time.sleep(5)
+        down_path, dataset_artifact = self.logger.download_dataset_artifact(dataset_name='dummy-set')
         assert os.path.exists(down_path)
         assert os.path.exists(os.path.join(down_path, "datapoint.tmp"))

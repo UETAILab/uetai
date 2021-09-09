5734b830d001b6697573a5c9180c67b26be2ccb9
Nguyen Văn Phi <5081237-gungui@users.noreply.gitlab.com>
2021-09-10 00:08:15 +0700

    increase coverage

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase
 
 from uetai.logger.general import check_requirements, check_python
@@ -5,7 +6,8 @@ from uetai.logger.general import check_requirements, check_python
 
 class TestGeneralFunctions(TestCase):
     def test_check_requirements(self):
-        check_requirements("./requirement.txt")
+        print(os.getcwd())
+        check_requirements("../requirements.txt")
 
     def test_check_python(self):
-        check_python
+        check_python(minimum="3.6")

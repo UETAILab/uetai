f56ccf3a8cbf4817e5dd4cfe130cce8cd9a5a630
manhdung20112000 <manhdung20112000@gmail.com>
2021-10-03 00:27:26 +0700

    Add validation monitoring + tests

--- src/uetai/callbacks/image_monitor.py
+++ src/uetai/callbacks/image_monitor.py
@@ -38,8 +38,11 @@ class ImageMonitorBase(Callback):
         self._log_every_n_steps = log_every_n_steps  # default is 50 in trainer.log_every_n_steps
         self._log_n_element_per_epoch = log_n_element_per_epoch  # default log all
         if self._on_epoch:
-            self._epoch: Dict[str, List] = {'images': [], 'ground_truths': [], 'predictions': []}
+            self._init_epoch()
             self._on_step = False
+
+    def _init_epoch(self):
+        self._epoch: Dict[str, List] = {'images': [], 'ground_truths': [], 'predictions': []}
 
     def on_train_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
         self._trainer = trainer
@@ -61,28 +64,10 @@ class ImageMonitorBase(Callback):
         In `add_image`, user should customize how they want image would be logged.
         In `ImageMonitorBase`, we only provide a simple logging demonstration.
         """
-        # log every n steps
-        if not self._log or (batch_idx + 1) % self._log_every_n_steps != 0:
+        if not self._log:
             return
-        compressed_batch: Dict[str, List] = {'images': [], 'ground_truths': [], 'predictions': []}
-        tensor, gt, _ = batch  # tensor, label, batch_size
-        # output must be Tensor or Dict ơf Tensor
-        if isinstance(outputs, Dict):
-            pred = outputs['pred']
-        elif isinstance(outputs, Tensor):
-            pred = outputs
-        else:
-            raise TypeError(f"Except `outputs` to be List or Dict, get {type(outputs)}")
-
-        for idx, image in enumerate(tensor):
-            transformed_image = transforms.ToPILImage()(image).convert("RGB")  # WxH dimension
-            compressed_batch['images'].append(transformed_image)  # batch_size x W x H dimension
-            compressed_batch['ground_truths'].append(gt[idx].item())
-            compressed_batch['predictions'].append(pred[idx].item())
-        if self._on_epoch:
-            for key, value in compressed_batch.items():
-                self._epoch[key] += value   # epoch:  number_of_data x W x H dimension
-        if self._on_step:
+        compressed_batch: Dict[str, Any] = self._extract_output_and_batch(outputs, batch)
+        if self._on_step and (batch_idx + 1) % self._log_every_n_steps == 0:
             self.add_image(tag='Train/media_step', media=compressed_batch)
 
     def on_train_epoch_end(
@@ -90,14 +75,9 @@ class ImageMonitorBase(Callback):
     ) -> None:
         if not self._on_epoch:
             return
-
-        assert len(self._epoch) > 0, f"Expect List of `tensor` or `np.array`, receive {len(self._epoch)} elements"
-        if self._log_n_element_per_epoch is None:
-            top_n_image = self._epoch  # default log all images (not recommend)
-        else:
-            top_n_image = {key: val[:self._log_n_element_per_epoch] for key, val in self._epoch.items()}
-        self.add_image(tag='Train/media_epoch', media=top_n_image)
-        self._epoch = {}  # reset List for next epoch
+        compressed_epoch = self._extract_epoch()
+        self.add_image(tag='Train/media_epoch', media=compressed_epoch)
+        self._init_epoch()
 
     def on_validation_batch_end(
         self,
@@ -108,10 +88,18 @@ class ImageMonitorBase(Callback):
         batch_idx: int,
         dataloader_idx: int,
     ) -> None:
-        pass
+        if not self._log:
+            return
+        compressed_batch: Dict[str, Any] = self._extract_output_and_batch(outputs, batch)
+        if self._on_step and (batch_idx + 1) % self._log_every_n_steps == 0:
+            self.add_image(tag='Valid/media_step', media=compressed_batch)
 
     def on_validation_epoch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
-        pass
+        if not self._on_epoch:
+            return
+        compressed_epoch = self._extract_epoch()
+        self.add_image(tag='Valid/media_epoch', media=compressed_epoch)
+        self._init_epoch()
 
     def on_keyboard_interrupt(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
         trainer_finish_run(trainer=trainer)
@@ -129,9 +117,11 @@ class ImageMonitorBase(Callback):
             predict = media['predictions'][idx]
             gt = media['ground_truths'][idx]
             if self._label_mapping is not None:
-                # TODO: check mapping valid
+                assert predict in self._label_mapping, f'Mapping dictionary is not including index {predict}'
+                assert gt in self._label_mapping, f'Mapping dictionary is not including index {gt}'
                 predict = self._label_mapping[predict]  # mapping label
-            images.append(wandb.Image(item, caption=f'Ground truth: {gt} Prediction: {predict}'))
+                gt = self._label_mapping[gt]
+            images.append(wandb.Image(item, caption=f'Gt: {gt} - Predict: {predict}'))
 
         self._add_image(tag=tag, images=images)
 
@@ -150,6 +140,34 @@ class ImageMonitorBase(Callback):
         if isinstance(logger, SummaryWriter) and 'wandb' in logger.log_tool:
             logger.wandb_run.log({tag: images})
 
+    def _extract_output_and_batch(self, outputs: Any, batch: Any):
+        compressed_batch: Dict[str, List] = {'images': [], 'ground_truths': [], 'predictions': []}
+        tensor, gt, _ = batch  # tensor, label, batch_size
+        # output must be Tensor or Dict ơf Tensor
+        if isinstance(outputs, Dict):
+            pred = outputs['pred']
+        elif isinstance(outputs, Tensor):
+            pred = outputs
+        else:
+            raise TypeError(f"Except `outputs` to be List or Dict, get {type(outputs)}")
+
+        for idx, image in enumerate(tensor):
+            transformed_image = transforms.ToPILImage()(image).convert("RGB")  # WxH dimension
+            compressed_batch['images'].append(transformed_image)  # batch_size x W x H dimension
+            compressed_batch['ground_truths'].append(gt[idx].item())
+            compressed_batch['predictions'].append(pred[idx].item())
+        if self._on_epoch:
+            for key, value in compressed_batch.items():
+                self._epoch[key] += value  # epoch:  number_of_data x W x H dimension
+        return compressed_batch
+
+    def _extract_epoch(self,):
+        assert len(self._epoch) > 0, f"Expect List of `tensor` or `np.array`, receive empty list"
+        if self._log_n_element_per_epoch is None:
+            return self._epoch  # default log all images (not recommend)
+
+        top_n_image = {key: val[:self._log_n_element_per_epoch] for key, val in self._epoch.items()}
+        return top_n_image
 
 # class ClassificationMonitor(ImageMonitorBase):
 #     def __init__(self, label_mapping, *args, **kwargs):
--- tests/test_image_callbacks.py
+++ tests/test_image_callbacks.py
@@ -12,6 +12,7 @@ from uetai.logger import SummaryWriter
 
 class TestImageCallbacks(unittest.TestCase):
     """Image callbacks test"""
+
     def __init__(self, *args, **kwargs):
         super(TestImageCallbacks, self).__init__(*args, **kwargs)
         self.logger = SummaryWriter('uetai', log_tool='wandb')
@@ -41,18 +42,24 @@ class TestImageCallbacks(unittest.TestCase):
         )
         monitor.on_train_start(trainer, pl_module=None)
         self.assertWarns(UserWarning)
+        monitor.on_train_epoch_end(trainer, None)
 
     @parameterized.expand([
-        (dict((key, torch.rand(10, requires_grad=True)) for key in ('loss', 'pred')), torch.rand(10, 3, 100, 100)),
+        (
+            dict((key, torch.rand(10, requires_grad=True)) for key in ('loss', 'pred')),
+            torch.rand(10, 3, 100, 100)
+        ),
         (torch.rand(10, requires_grad=True), torch.rand(10, 3, 100, 100)),
     ])
     def test_training_image_monitor(self, outputs, images):
-        self.training_image_log(outputs=outputs, images=images)
+        # mapping = {i: str(i) for i in range(10)}
+        # monitor = ImageMonitorBase(label_mapping=mapping)
+        self.pipeline_image_monitor(outputs=outputs, images=images)
 
     @parameterized.expand([
         (ImageMonitorBase(on_step=True), ),
         (ImageMonitorBase(on_epoch=True), ),
-        (ImageMonitorBase(on_epoch=True, log_n_element_per_epoch=2), ),
+        (ImageMonitorBase(on_epoch=True, log_n_element_per_epoch=2),),
     ])
     def test_training_callbacks_by_epoch_n_step(self, monitor=None):
         sample_images = torch.rand(10, 3, 100, 100)
@@ -61,7 +68,7 @@ class TestImageCallbacks(unittest.TestCase):
             logger=self.logger,
             callbacks=[monitor]
         )
-        self.training_image_log(
+        self.pipeline_image_monitor(
             images=sample_images, outputs=sample_outputs, monitor=monitor, trainer=trainer
         )
 
@@ -71,9 +78,9 @@ class TestImageCallbacks(unittest.TestCase):
     ])
     def test_training_image_monitor_xfail(self, outputs, images, expectation):
         with self.assertRaises(expectation):
-            self.training_image_log(outputs=outputs, images=images)
+            self.pipeline_image_monitor(outputs=outputs, images=images)
 
-    def training_image_log(self, outputs, images, monitor=None, trainer=None):
+    def pipeline_image_monitor(self, outputs, images, monitor=None, trainer=None):
         if monitor is None:
             monitor = ImageMonitorBase(log_every_n_steps=1)
         if trainer is None:
@@ -92,3 +99,11 @@ class TestImageCallbacks(unittest.TestCase):
             dataloader_idx=0
         )
         monitor.on_train_epoch_end(trainer, None)
+        monitor.on_validation_batch_end(
+            trainer, None,
+            batch=example_data,
+            outputs=outputs,
+            batch_idx=0,
+            dataloader_idx=0
+        )
+        monitor.on_validation_epoch_end(trainer, None)

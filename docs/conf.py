d4cd28cb0dd3f516383b8a68d5d4a4754508c117
Phi Nguyễn <phi.nguyen.uet@gmail.com>
2021-10-09 11:08:00 +0700

    ignore change

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,6 @@ except Exception as e:
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    "sphinx_rtd_theme",
     "sphinx.ext.githubpages",
     'sphinx.ext.viewcode',
     "sphinx_typo3_theme",

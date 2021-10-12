ccec135e649c8fa777d95829d7466cb4d6115a19
manhdung20112000 <manhdung20112000@gmail.com>
2021-10-12 15:04:50 +0700

    Remove language callbacks

--- src/uetai/callbacks/__init__.py
+++ src/uetai/callbacks/__init__.py
@@ -1,12 +1,8 @@
 """init public callbacks"""
-from .language_monitor import TextMonitorBase
 from .image_monitor import ImageMonitorBase, ClassificationMonitor
 
 __all__ = [
     # vision
     "ImageMonitorBase",
     "ClassificationMonitor",
-
-    # language
-    "TextMonitorBase",
 ]
